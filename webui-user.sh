eb6e4b8dd26227317ba51ac51883058eb6a5e711
hydrian <ben.tyger@tygerclan.net>
2023-04-25 17:49:19 -0400

    Updated TORCH_COMMAND to use ROCm 5.4.2 libraries

--- webui-user.sh
+++ webui-user.sh
@@ -25,7 +25,7 @@
 #export LAUNCH_SCRIPT="launch.py"
 
 # install command for torch
-#export TORCH_COMMAND="pip install torch==1.12.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113"
+export TORCH_COMMAND="pip3 install torch torchvision --index-url https://download.pytorch.org/whl/rocm5.4.2"
 
 # Requirements file to use for stable-diffusion-webui
 #export REQS_FILE="requirements_versions.txt"
